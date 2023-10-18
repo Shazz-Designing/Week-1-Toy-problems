fd70ca16b62430dc6d77523332a516261f9ebf3d
sharon <sapot@safaricom.co.ke>
2023-10-18 22:25:10 +0300

    Add code for student grade generator

---
+++ solutions/student-grade-generator.js
@@ -0,0 +1,19 @@
+function calculateGrade() {
+    const marks = parseFloat(prompt("Enter student marks:"));
+  
+    if (marks > 79) {
+      return "A";
+    } else if (marks >= 60 && marks <= 79) {
+      return "B";
+    } else if (marks >= 50 && marks <= 59) {
+      return "C";
+    } else if (marks >= 40 && marks <= 49) {
+      return "D";
+    } else {
+      return "E";
+    }
+  }
+  
+  const grade = calculateGrade();
+  console.log(`Grade: ${grade}`);
+  
